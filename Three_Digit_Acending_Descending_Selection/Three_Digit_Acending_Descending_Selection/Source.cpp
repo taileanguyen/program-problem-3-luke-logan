533f03dd6b2e9b653264e6a09badd65ff4950f7a
taileanguyen <31855572+taileanguyen@users.noreply.github.com>
2017-10-16 09:56:38 -0500

    Assignment 4 Tailea Nguyen
    10/16/17

--- Three_Digit_Acending_Descending_Selection/Three_Digit_Acending_Descending_Selection/Source.cpp
+++ Three_Digit_Acending_Descending_Selection/Three_Digit_Acending_Descending_Selection/Source.cpp
@@ -1,42 +1,43 @@
-/*
-Luke Hage, Logan Smith
-Three_Digit_Ascending_Descending_Selection
-Display ascending, descending or neither based on user's 3-digit number
-*/
-
-//Libraries
 #include <iostream>
 #include <conio.h>
 
-//Namespace
-using namespace std;
+// Editor Tailea Nguyen
+using namespace std; //*
 
-//Functions
-
+					//functions()
 void pause() {
 	cout << "Press any key to continue . . .";
 	while (!_kbhit());
 	_getch();
 	cout << '\n';
 }
-
 //MAIN
 void main() {
-	int x;
-	cout << "Input 3 digit positive integer. . ." << endl;
-	cin >> x;
-	int a = (x / 100);
-	int b = (x % 100) / 10;
-	int c = (x % 10);
-	if (a < b && b < c) {
+	for (int i = 0; i < 30; i++){ // Repeats only 30 times
+	
+	int someThreeDigitNumber; // The number the user inputs
+	cout << "Input 3 digit positive integer. . .";
+	cin >> someThreeDigitNumber;
+	int a, b, c;
+
+	a = (someThreeDigitNumber / 100); // Defines the first digit
+	b = (someThreeDigitNumber / 100) % 10; // Defines the second digit
+	c = (someThreeDigitNumber % 10); // Defines the third digit
+
+
+
+	if (a < b && b < c) { // check to see if ascending
+
 		cout << "Your digits are in ascending order from left to right." << endl;
-		}
-	else if (a > b && b > c) {
+	}
+	else if (a > b && b > c) { // check to see if descending
+
 		cout << "Your digits are in descending order from left to right." << endl;
 	}
-	else{
+	else if (a = b = c) { // check to see if neither
 		cout << "Your digits are neither ascending or descending." << endl;
 	}
-	pause();
+	}
+	pause(); // pauses to see the displayed text
 
 }
